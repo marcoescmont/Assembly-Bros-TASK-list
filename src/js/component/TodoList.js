596a8bd85632366852a00252613bace4d09d69fa
marcoescmont <marcoescmont@gmail.com>
2021-07-15 01:31:54 +0000

    first commit

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -2,7 +2,43 @@ import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 const TodoList = () => {
-	const [tasks, setTasks] = useState(["New task"]);
+    const [tasks, setTasks] = useState(["New task"]);
+    
+    	const fetchTodo = () => {
+
+    //  fetch("https://assets.breatheco.de/apis/fake/todos/user/marcoescmont")
+		// .then(response => response.json())
+		// .then(todos => console.log(todos))
+        // .catch(error => console.log(error));
+            
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/marcoescmont", {
+			method: "PUT",
+			body: JSON.stringify(""),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+    };
+    
+    
+
+	useEffect(() => {
+		fetchTodo();
+	}, []);
 
 	const inputRef = useRef(null);
 
