9fa4e074dbd07102bfb631ce1673963da1411d75
marcoescmont <marcoescmont@gmail.com>
2021-10-26 00:20:05 +0000

    fixing CSS

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { getList, updateList, createList, deleteList } from "./api";
 import PropTypes from "prop-types";
+import ABlogo from "/ABlogo.png";
 
 const TodoList = () => {
 	const [tasks, setTasks] = useState([]);
@@ -62,29 +63,45 @@ const TodoList = () => {
 	}, []);
 
 	return (
-		<div className="listBody mx-auto mt-4">
-			<h2 className="text-center">What{"'"}s up to task</h2>
-			<input
-				className="border-0 mx-2 mt-1 black-bg text-white w-75"
-				type="text"
-				id="fname"
-				name="fname"
-				onChange={e => setInput(e.target.value)}
-				value={input}
-				placeholder="Add your task..."
-				onKeyDown={newTask}
-				ref={inputRef}
-			/>
-			<ul className="list-group py-2"> {makeList}</ul>
-			<p className="bg-dark border-round pl-2 d-flex justify-content-between">
-				{tasks.length} Items left
-				<button
-					type="button"
-					className="btn bg-transparent text-white"
-					onClick={deleteList}>
-					<i className="fas fa-minus-circle"></i>
-				</button>
-			</p>
+		<div>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a
+					className="navbar-brand d-flex justify-content-start"
+					href="#">
+					<img
+						alt="AB Logo"
+						width="50px"
+						height="50px"
+						src={ABlogo}
+					/>
+					{"   "}
+					<h2>Assembly Bros</h2>
+				</a>
+			</nav>
+			<div className="listBody mx-auto mt-4">
+				<h2 className="text-center">Help your client with...</h2>
+				<input
+					className="border-0 mx-2 mt-1 black-bg text-white w-75"
+					type="text"
+					id="fname"
+					name="fname"
+					onChange={e => setInput(e.target.value)}
+					value={input}
+					placeholder="Add your task..."
+					onKeyDown={newTask}
+					ref={inputRef}
+				/>
+				<ul className="list-group py-2"> {makeList}</ul>
+				<p className="bg-dark border-round pl-2 d-flex justify-content-between">
+					{tasks.length} Items left
+					<button
+						type="button"
+						className="btn bg-transparent text-white"
+						onClick={deleteList}>
+						<i className="fas fa-minus-circle"></i>
+					</button>
+				</p>
+			</div>
 		</div>
 	);
 };
